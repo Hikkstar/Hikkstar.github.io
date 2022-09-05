fb8b0616bf25eecd980717961bff848f6b8bcffb
Hikkstar <54229046+Hikkstar@users.noreply.github.com>
2022-09-05 14:31:49 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ ham.on('click', function () {
   ham.toggleClass('active'); 
   nav.toggleClass('active'); 
 });
+
+$('#page-top').click(function () {
+  $('body,html').animate({
+      scrollTop: 0
+  }, 500);
+  return false;
+});
